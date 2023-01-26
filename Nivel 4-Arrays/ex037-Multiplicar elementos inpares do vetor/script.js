b90bb506d064cb294931af0f2a73d1e26004bdd8
Pedro Henrique Andreatti <106180196+Pedro-devs@users.noreply.github.com>
2023-01-26 07:48:47 -0300

    Add files via upload

---
+++ Nivel 4-Arrays/ex037-Multiplicar elementos inpares do vetor/script.js
@@ -0,0 +1,21 @@
+function multiplicar() {
+    let saida = document.getElementById("saida")
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+    let n3 = Number(document.getElementById("n3").value)
+    let n4 = Number(document.getElementById("n4").value)
+    let num = Number(document.getElementById("num").value)
+
+    let vetor = [n1, n2, n3, n4]
+    saida.innerHTML += `<h2>Resultado </h2>`
+    for (let i = 0; i < vetor.length; i++) {
+
+        if (vetor[i] % 2 != 0) {
+            saida.innerHTML += `<p>v[${i + 1}] X  ${num} = ${vetor[i] * num} </p>`
+        }
+
+    }
+
+
+
+}
