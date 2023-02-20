54a804918b8a23d623f93a678be185d6e25759f1
Pedro Henrique Andreatti <106180196+Pedro-devs@users.noreply.github.com>
2023-02-20 15:54:41 -0300

    Delete Aprendendo Node.js directory

--- Aprendendo Node.js/003-Primeira aplicação web, HTTP com Node.js/script.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");//nao existe outro arquivo que se chama http pra importar 
-                           //mas esse modulo http ele ja vem no proprio node.js 
-http.createServer(function(requisicao,resposta){//requisição aonde o usuario pede ao servidor  e o servidor dá a resposta
-    resposta.end("<h1>Bem vindo ao meu site !</h1>")
-    
-}).listen(3000);
-
-console.log("Meu servidor está rodando!");//menssagem pra saber se o servidor está rodando
